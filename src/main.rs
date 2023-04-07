205c3cf50d404b644bdfe78195543aa7069cea0b
MooreMachine <8643188+MooreMachine@users.noreply.github.com>
2023-04-07 20:31:47 +0200

    Define the structure of a question.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::str::FromStr;
+
+use q_and_a_web_service::model::question::{Question, QuestionId};
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of Question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+    println!("{:?}", question);
 }
